7a265f81500a7d3251a592a936e72430fef0173d
Loi Van Tran <datloiboi@gmail.com>
2021-04-09 15:40:45 +0700

    Rename lab

--- src/App.js
+++ src/App.js
@@ -93,7 +93,7 @@ const Navbarr = (props) => {
   const [email, setEmail] = useState('')
   return (
     <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar.Brand href="#home">Prop Drilling</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
